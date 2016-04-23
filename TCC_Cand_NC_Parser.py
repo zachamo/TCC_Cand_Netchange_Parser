7b93e394c6154eb5c301a6d83f73aba10eeb7296
zachamo <zachamo@hotmail.com>
2016-04-22 20:40:26 -0400

    Update TCC_Cand_NC_Parser.py

--- TCC_Cand_NC_Parser.py
+++ TCC_Cand_NC_Parser.py
@@ -7,9 +7,12 @@ MESSAGES = ['total.candidate=','total.candidate.delete=','total.candidate.create
             'total.status.error=']
 
 def csv_extract(filename):
-    """ (file open for reading) -> output file
-    Check for specified data in csv, extract into output
+    """ (file open for reading) -> list of str, list of str,int
+    Check file for specified MESSAGES, extract the corresponding result numbers 
+    in the TCC result csv at the same index as the message, also extract any 
+    errors found in the result contents and a count of lines processed.
     """
+    
     result_nums = []
     for i in range(len(MESSAGES)):
         result_nums.append('0')
@@ -25,45 +28,55 @@ def csv_extract(filename):
             errors.append(line.strip())
     return result_nums,errors,line_count
 
+
 def list_to_nums(list_to_convert):
     """ (list of num as str) -> str
-    remove quotations from provided str
+    Converts a list to a comma-seperated string by removing brackets and 
+    single-quotes from provided str
     """
+    
     output = ''
     for c in str(list_to_convert)[1:-1]:
         if c != "'":
             output += c
     return output
     
+    
 def read_and_write():
     """(Nonetype)
-    Read each file, find MESSAGES, write output.
+    Open each file in FILE_LIST, perform a csv_extract, then write the results
+    to OUTPUT_NAME.
     """
+    
     output = open(OUTPUT_NAME,'a')
     
     for file in FILE_LIST:
         result_nums,errors,line_count = csv_extract(file)
         output.write(file+'\n')
-        
+        print(file,"lines processed:",line_count)
+
+        #Specific to our process which includes a 'Scrubber' script
         if "candidate_scrubber" in file:
-             output.write("Record count: "+str(line_count-1)+'\n')
-             
+            output.write("Record count: "+str(line_count-1)+'\n')
+    
+        #This is for the Netchange Results file
         else:
             output.write(str(MESSAGES)[1:-1]+'\n')
             output.write(list_to_nums(result_nums)+'\n')
         
+        #Append each full error line to the output file
         output.write('Errors Below:\n')
         for error in errors:
             output.write(error+'\n')
-            
         output.write('\n')
         
     output.close()
 
 
 #Begin Program
-print ("Files_to_analyze:",FILE_LIST)
+print ("This program analyses all csv files in the current folder")
+print ("Files_to_analyze: ",FILE_LIST)
 proceed = input('Proceed: Y/N: ')
 if proceed in ("y","Y"):
     read_and_write()
-print('Complete')
+print('Complete, see',OUTPUT_NAME)
